3e6acaf2437a298dd09aec44440e020d5b0e1270
Alx-john <john.ngugi6@yahoo.com>
2022-06-07 14:16:36 +0300

    simple shell

--- _freeAppData.c
+++ _freeAppData.c
@@ -25,10 +25,14 @@ void _freeAppData(appData_t *prmData)
 	if (prmData->history != NULL)
 		_freeCharDoublePointer(prmData->history);
 	prmData->history = NULL;
-	/*if (prmData->env != NULL)
-		_freeEnvList(prmData->env);
-	prmData->env = NULL;*/
-	/*if (prmData != NULL)
-		free(prmData);
-	prmData = NULL;*/
+	/**
+	 * if (prmData->env != NULL)
+	 * _freeEnvList(prmData->env);
+	 * prmData->env = NULL;
+	 */
+	/**
+	 * if (prmData != NULL)
+	 * free(prmData);
+	 * prmData = NULL;
+	 */
 }
